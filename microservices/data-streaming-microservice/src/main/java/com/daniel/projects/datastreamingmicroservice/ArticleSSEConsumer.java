c2a3aa23183fc37767f5804a899166aea462c962
Daniel Lee <dblee0532@gmail.com>
2023-03-10 20:51:17 -0500

    using reactor core Flux and Sinks for SSE on endpoint /stream/articles. Not sure if this is the best way but works

--- microservices/data-streaming-microservice/src/main/java/com/daniel/projects/datastreamingmicroservice/ArticleSSEConsumer.java
+++ microservices/data-streaming-microservice/src/main/java/com/daniel/projects/datastreamingmicroservice/ArticleSSEConsumer.java
@@ -3,13 +3,26 @@ package com.daniel.projects.datastreamingmicroservice;
 import com.daniel.projects.datastreamingmicroservice.model.Article;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 @Component
 public class ArticleSSEConsumer {
+
+    private final Sinks.Many<Article> sink;
+
+    public ArticleSSEConsumer() {
+        this.sink = Sinks.many().multicast().onBackpressureBuffer();
+    }
 
     @KafkaListener(topics = "articles", groupId = "article-sse-group",
             containerFactory = "sseListenerContainerFactory")
     public void consumeAndSSE(Article article) {
         System.out.println("SSE-GROUP SUCCESS: " + article.getTitle());
+        sink.tryEmitNext(article);
+    }
+
+    public Flux<Article> getFlux() {
+        return sink.asFlux();
     }
 }
---
+++ microservices/data-streaming-microservice/src/main/java/com/daniel/projects/datastreamingmicroservice/controller/ArticleStreamController.java
@@ -0,0 +1,27 @@
+package com.daniel.projects.datastreamingmicroservice.controller;
+
+import com.daniel.projects.datastreamingmicroservice.ArticleSSEConsumer;
+import com.daniel.projects.datastreamingmicroservice.model.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(value = "/stream")
+public class ArticleStreamController {
+
+    private final ArticleSSEConsumer articleSSEConsumer;
+
+    @Autowired
+    public ArticleStreamController(ArticleSSEConsumer articleSSEConsumer) {
+        this.articleSSEConsumer = articleSSEConsumer;
+    }
+
+    @GetMapping(value = "/articles", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Article> getArticles() {
+        return articleSSEConsumer.getFlux();
+    }
+}
